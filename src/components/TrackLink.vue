0ad3a00f17b6c4303c75edf0edd88d84435c5b47
Justas Bieliauskas <justyvas0@gmail.com>
2019-05-09 22:25:25 +0300

    Numberis -> Numeris.

--- src/components/TrackLink.vue
+++ src/components/TrackLink.vue
@@ -64,7 +64,7 @@ export default {
         Vardas: this.name,
         Suma: this.amount,
         Marke: this.carBrand,
-        Numberis: this.carNumber,
+        Numeris: this.carNumber,
         Sablonas: this.template,
       });
     },
